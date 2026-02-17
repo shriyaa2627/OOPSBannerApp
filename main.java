ef740622d8c90c4af27462c431558028cb0ba302
shriyaa2627 <shriyas2602@gmail.com>
2026-02-17 09:44:49 +0530

    Print OOPS

---
+++ main.java
@@ -0,0 +1,22 @@
+/**
+ * OOPSBannerApp UC2 - OOPS BAnner Display APllication
+ * 
+ * This calss demonstrates a simple Java application that displays the Object Oriented Programming System OOPS acronym to the console.
+ * 
+ * @author Allen Paul D
+ * @version 1.0
+ */
+
+public class OOPSBannerApp
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("  **   " + "   **   " + "  **  " + "  ** ");
+        System.out.println(" *  *  " + "  *  *  " + " *   * " + " **     ");
+        System.out.println("*    * " + " *    * " + " *   * " + " **     ");
+        System.out.println("*    * " + " *    * " + "  **  " + "  ** ");
+        System.out.println("*    * " + " *    * " + " *      " + "      *");
+        System.out.println(" *  *  " + "  *  *  " + " *      " + "      *");
+        System.out.println("  **   " + "   **   " + " *      " + "  ** ");
+    }
+}
