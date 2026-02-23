b815ec23198ec73c8678537a12eafc289bcd1bf0
shriyaa2627 <shriyas2602@gmail.com>
2026-02-23 08:10:05 +0530

    main.java

---
+++ main.java
@@ -0,0 +1,13 @@
+class OOPSBannerApp
+  {
+    public static void main(String[]arge)
+    {
+System.out.println("  **   " + "   " + "  ** ");
+System.out.println(" *  *  " + "   " + " **      ");
+System.out.println("*    * " + "   " + " **      ");
+System.out.println("*    * " + "   " + "  ***  ");
+System.out.println("*    * " + "   " + "      ** ");
+System.out.println(" *  *  " + "   " + "      ** ");
+System.out.println("  **   " + "   " + " ** ")
+  }
+  }    
